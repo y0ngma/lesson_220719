1d9d113b39f1451a73de696142a39a3e948b2ca9
y0ngma <takeit2sy@pusan.ac.kr>
2022-11-11 18:04:47 +0900

    연습중

--- multi_ROC.py
+++ multi_ROC.py
@@ -35,33 +35,33 @@ classifier = OneVsRestClassifier(
 )
 y_score = classifier.fit(X_train, y_train).decision_function(X_test)
 
-# Compute ROC curve and ROC area for each class
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-    roc_auc[i] = auc(fpr[i], tpr[i])
+# # Compute ROC curve and ROC area for each class
+# fpr = dict()
+# tpr = dict()
+# roc_auc = dict()
+# for i in range(n_classes):
+#     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+#     roc_auc[i] = auc(fpr[i], tpr[i])
 
-# Compute micro-average ROC curve and ROC area
-fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+# # Compute micro-average ROC curve and ROC area
+# fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+# roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
-# Plot of a ROC curve for a specific class
-plt.figure()
-lw = 2
-plt.plot(
-    fpr[2],
-    tpr[2],
-    color="darkorange",
-    lw=lw,
-    label="ROC curve (area = %0.2f)" % roc_auc[2],
-)
-plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("Receiver operating characteristic example")
-plt.legend(loc="lower right")
-plt.show()
+# # # Plot of a ROC curve for a specific class
+# # plt.figure()
+# # lw = 2
+# # plt.plot(
+# #     fpr[2],
+# #     tpr[2],
+# #     color="darkorange",
+# #     lw=lw,
+# #     label="ROC curve (area = %0.2f)" % roc_auc[2],
+# # )
+# # plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+# # plt.xlim([0.0, 1.0])
+# # plt.ylim([0.0, 1.05])
+# # plt.xlabel("False Positive Rate")
+# # plt.ylabel("True Positive Rate")
+# # plt.title("Receiver operating characteristic example")
+# # plt.legend(loc="lower right")
+# # plt.show()
